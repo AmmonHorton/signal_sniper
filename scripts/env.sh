a42397d4db4b43fd4892b85071393941a8477ce7
AmmonHorton <aj_horton@hotmail.com>
2025-02-20 00:27:08 -0700

    Corrected make and make celan alias

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,3 +1,3 @@
-alias mkf="cd build && make && make install && cd -"
-alias mk_clean="deactivate && cd build && make cln && cd - && rm -rf build && rm -rf .venv"
+alias mkf="cd $(git rev-parse --show-toplevel)/build && make && make install; cd -"
+alias mk_clean="deactivate && cd $(git rev-parse --show-toplevel) && rm -rf build && rm -rf .venv; cd -"
 source .venv/bin/activate
